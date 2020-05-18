850d23ed8d76498b7d4eeb0f8f430daa58245897
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-05-18 16:22:55 +0300

    add feature length

--- src/App.java
+++ src/App.java
@@ -1,13 +1,25 @@
 public class App {
     public static void main(String[] args) {
-        try {
-            int first = Integer.parseInt(args[0]);
-            int second = Integer.parseInt(args[1]);
-            Fibonacci f = new Fibonacci(first, second);
-            f.fibbonacci();
-        } catch (Exception ex) {
-            System.out.println("Please enter the number");
-            System.exit(1);
+        if (args.length == 2) {
+            try {
+                int first = Integer.parseInt(args[0]);
+                int second = Integer.parseInt(args[1]);
+                Fibonacci f = new Fibonacci(first, second);
+                f.output_fibonacciToNumber();
+            } catch (Exception ex) {
+                System.out.println("Please enter the number");
+                System.exit(1);
+            }
+        } else {
+            try {
+                int first = Integer.parseInt(args[0]);
+                int second = 0;
+                Fibonacci f = new Fibonacci(first, second);
+                f.outputFibonacci();
+            } catch (Exception ex) {
+                System.out.println("Please enter the number");
+                System.exit(1);
+            }
         }
     }
 }
--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -7,7 +7,7 @@ public class Fibonacci {
         this.secondNumber = secondNumber;
     }
 
-    public void fibbonacci() {
+    public void output_fibonacciToNumber() {
         int sec = firstNumber + 1;
         int stop = secondNumber;
         int result = 0;
@@ -17,6 +17,43 @@ public class Fibonacci {
                 System.out.print(firstNumber + ", ");
                 firstNumber = sec;
                 sec = result;
+            }
+        }
+    }
+
+    public void outputFibonacci(){
+        if (firstNumber >= 0 && firstNumber <= 9) {
+            int sec = firstNumber + 1;
+            int result = 0;
+            for (int i = firstNumber; i <= 9; i++) {
+                result = firstNumber + sec;
+                if (firstNumber <= 9) {
+                    System.out.print(firstNumber + ", ");
+                    firstNumber = sec;
+                    sec = result;
+                }
+            }
+        } else if (firstNumber >= 10 && firstNumber <= 99) {
+            int sec = firstNumber + 1;
+            int result = 0;
+            for (int i = firstNumber; i <= 99; i++) {
+                result = firstNumber + sec;
+                if (firstNumber <= 99) {
+                    System.out.print(firstNumber + ", ");
+                    firstNumber = sec;
+                    sec = result;
+                }
+            }
+        } else if (firstNumber >= 100 && firstNumber <= 999) {
+            int sec = firstNumber + 1;
+            int result = 0;
+            for (int i = firstNumber; i <= 999; i++) {
+                result = firstNumber + sec;
+                if (firstNumber <= 999) {
+                    System.out.print(firstNumber + ", ");
+                    firstNumber = sec;
+                    sec = result;
+                }
             }
         }
     }
